3d8b237a8d39dc96430f46b370ee9d0337e77563
carllos39 <carllos2005@outlook.com.br>
2023-10-25 15:00:59 -0300

    formspree

--- js/contato.js
+++ js/contato.js
@@ -134,3 +134,35 @@ botaoBuscar.addEventListener("click", async function (event) {
 });
 
 */
+//Programação do formspree
+
+
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: formulario.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "Seus dados foram enviados com sucesso!";
+      formulario.reset()
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+          status.innerHTML = "Deu ruim! Algo de errado aconteceu"
+        }
+      })
+    }
+  }).catch(error => {
+    status.innerHTML = "Deu ruim! Algo de errado aconteceu"
+  });
+}
+formulario.addEventListener("submit", handleSubmit)
